80a6d5c069226d26d31c821dbe67bbe45e84c732
Victor Badia <victorbadia100@gmail.com>
2024-12-23 02:40:24 -0500

    Update BCD_SevenSeg.v

--- sources1/BCD_SevenSeg.v
+++ sources1/BCD_SevenSeg.v
@@ -23,7 +23,8 @@ module BCD_SevenSeg(
     input [3:0] A,
     output [6:0] D
     );
-    
+
+    // BCD to Hex, from logic minimized truth table
     assign D[0] = (~A[3] & ~A[2] & ~A[1] & A[0]) |
                   (~A[3] & A[2] & ~A[1] & ~A[0]) |
                   (A[3] & A[2] & ~A[1] & A[0]) |
@@ -68,14 +69,26 @@ module top (
     reg [1:0] mux_sel;
     reg [16:0] refresh_counter;
     reg clk_div;
+
+    // Instantiation oand wiring of each display
+    BCD_SevenSeg seg1 (.A(sw[3:0]),   
+                       .D(disp1)
+                      );
     
-    BCD_SevenSeg seg1 (.A(sw[3:0]),   .D(disp1));
-    BCD_SevenSeg seg2 (.A(sw[7:4]),   .D(disp2));
-    BCD_SevenSeg seg3 (.A(sw[11:8]),  .D(disp3));
-    BCD_SevenSeg seg4 (.A(sw[15:12]), .D(disp4));
+    BCD_SevenSeg seg2 (.A(sw[7:4]),   
+                       .D(disp2)
+                      );
+    
+    BCD_SevenSeg seg3 (.A(sw[11:8]),  
+                       .D(disp3)
+                      );
+    
+    BCD_SevenSeg seg4 (.A(sw[15:12]), 
+                       .D(disp4)
+                      );
 
 
-    // Clock Divider to create a slower clock (e.g., 1 kHz from 100 MHz)
+    // Clock Divider to create a slower clock
     always @(posedge clk) begin
         if (refresh_counter == 99_999) begin
             clk_div <= ~clk_div; // Toggle the clock signal every 100,000 cycles
@@ -85,12 +98,12 @@ module top (
         end
     end
 
-    // Multiplexing logic based on the divided clock signal
+    // Binary increment of mux_sel based on clk 
     always @(posedge clk_div) begin
         mux_sel <= mux_sel + 1;  // Increment the multiplexing selection every clock pulse
     end
 
-    // Select which display to activate based on the select signal of multiplexer
+    // Cycling through each display based on clk
     always @(*) begin
         case (mux_sel)
             2'b00: begin
